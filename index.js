a88b8555da3950495ebc0310e1842f461c0f02d4
José Lara <josselara1@gmail.com>
2023-03-23 20:05:46 -0300

    a

--- index.js
+++ index.js
@@ -12,13 +12,11 @@
   const port = process.env.PORT || 3001;
   const app = express();
 
-  app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+  app.use(
+      cors({
+        origin: "*",
+      })
+    );
   app.use( bodyParser.json( { limit: '50mb' } ) );
   app.use( bodyParser.urlencoded( { limit: '50mb', extended: true } ) );
   app.use( cookieParser() )

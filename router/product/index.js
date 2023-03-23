34a9697fc93430f44c47324c36eab031a7cada05
José Lara <josselara1@gmail.com>
2023-03-23 20:06:07 -0300

    n

--- router/product/index.js
+++ router/product/index.js
@@ -5,5 +5,5 @@ export const routerProduct = Router();
 
 
 routerProduct.post("/",getProduct)
-routerProduct.get("/",(req,res)=>res.send('1221')))
+routerProduct.get("/",(req,res)=>res.send('1221'))
 // routerProduct.get("/",getProduct)
